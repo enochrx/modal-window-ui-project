9e87194fc1ee0fe0a135f54789f3d311ad73d9d2
enochrx <drreayy@gmail.com>
2022-06-13 23:35:23 +0100

    declared

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 'use strict';
 
-const btnsOpenModal = document.querySelector('.show-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
 const btnCloseModal = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
 const modal = document.querySelector('.modal');
 
-console.log();
+console.log(btnsShowModal);
+
+for (let i = 0; i < btnsShowModal.length; i++)
+  btnsShowModal[i].addEventListener('click', function () {
+    console.log('click button');
+  });
