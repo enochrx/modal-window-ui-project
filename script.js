582f644c0890ddc6ac12aff8f7ed1a0dbc239b03
enochrx <drreayy@gmail.com>
2022-06-13 15:12:12 +0100

    DOM

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const btnsOpenModal = document.querySelector('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+
+console.log();
