3fd21f202f6d10163fb0d7f252d60e572ea7dbd9
enochrx <drreayy@gmail.com>
2022-06-15 07:39:14 +0100

    added ftn

--- script.js
+++ script.js
@@ -7,7 +7,20 @@ const modal = document.querySelector('.modal');
 
 console.log(btnsShowModal);
 
+const openModal = function () {
+  console.log('click button');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
 for (let i = 0; i < btnsShowModal.length; i++)
-  btnsShowModal[i].addEventListener('click', function () {
-    console.log('click button');
-  });
+  btnsShowModal[i].addEventListener('click', openModal);
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
